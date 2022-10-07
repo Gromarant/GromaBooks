b1dc40ae1ca5d0515ed408ff2bfe4bb6d24cec74
Mariangelica Rodriguez <gromarant@gmail.com>
2022-10-08 00:49:08 +0200

    Update of the project

--- js/main.js
+++ js/main.js
@@ -1,100 +1,115 @@
 'use strict';
 
-let bookOnemage = 'https://images-na.ssl-images-amazon.com/images/I/61RRViAYqdL.jpg';
-let bookOneName = 'Código Limpio';
-let bookOneAutor = 'Robert C. Martin';
-let bookOneDescipt = 'Clean Code, o Código Limpio, es una filosofía de desarrollo de software que consiste en aplicar técnicas simples que facilitan la escritura y lectura de un código, volviéndolo más fácil de entender';
+let books = [
+  {
+    title: 'Código Limpio',
+    autor: 'Robert C. Martin',
+    description: 'Clean Code, o Código Limpio, es una filosofía de desarrollo de software que consiste en aplicar técnicas simples que facilitan la escritura y lectura de un código, volviéndolo más fácil de entender',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/61RRViAYqdL.jpg',
+  },
+  {
+    title: 'Código Limpio',
+    autor: 'Robert C. Martin',
+    description: 'Clean Code, o Código Limpio, es una filosofía de desarrollo de software que consiste en aplicar técnicas simples que facilitan la escritura y lectura de un código, volviéndolo más fácil de entender',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/61RRViAYqdL.jpg',
+  },
+  {
+    title: 'Código Limpio',
+    autor: 'Robert C. Martin',
+    description: 'Clean Code, o Código Limpio, es una filosofía de desarrollo de software que consiste en aplicar técnicas simples que facilitan la escritura y lectura de un código, volviéndolo más fácil de entender',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/61RRViAYqdL.jpg',
+  },
+];
 
-let bookTwomage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPgCOMTY0J8QmCQeb9Ini92f_OsLVdY0Y--w&usqp=CAU';
-let bookTwoName = 'Fiona';
-let bookTwoAutor = 'British Shorthair';
-let bookTwoDescipt = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!';
-
-let bookThreemage = 'https://www.librerialunanueva.com/imagenes/9788441/978844154499.JPG';
-let bookThreeName = 'Cielo';
-let bookThreeAutor = 'British Shorthair';
-let bookThreeDescipt = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!';
-
-const itemCard = document.querySelector('.book-data');
-const bookOne = `<li class="card">
-<article>
-  <img class="card-img" src=${ bookOnemage } alt="código limpio"/>
-  <h3 class="card-title"><strong>${ bookOneName }:</strong> Manual de estilo para el desarrollo ágil de software</h3>
-  <h4 class="card-autor">${ bookOneAutor }</h4>
-  <p class="card-description">${ bookOneDescipt }</p>
-</article>
-</li>`;
-
-const bookTwo = `<li class="card">
-<img class="card-img" src=${ bookTwomage } alt="book"/>
-<h3 class="card-title">${ bookTwoName }</h3>
-<h4 class="card-autor">${ bookTwoAutor }</h4>
-<p class="card-description">${ bookTwoDescipt }</p>
-</li>`
-
-const bookThree = `<li class="card">
-<img class="card-img" src=${ bookThreemage } alt="book"/>
-<h3 class="card-title">${ bookThreeName }</h3>
-<h4 class="card-autor">${ bookThreeAutor }</h4>
-<p class="card-description">${ bookThreeDescipt }</p>
-</li>`
-
-itemCard.innerHTML = bookOne + bookTwo + bookThree;
-
-const searchDescriptInput = document.querySelector('.search-description');
-searchDescriptInput.classList.add('.js_in_search_desc');
-searchDescriptInput.value = '';
-const descriptText = searchDescriptInput.value;
-
-const resultList = document.querySelector('.list');
-const searchByDescript = ( descriptText ) => {
-  if(bookOneDescipt.includes( descriptText )) {
-    return resultList = bookOne;
-  } 
-  if(bookTwoDescipt.includes( descriptText )) {
-    return resultList = bookTwo;
-  } 
-  if(bookThreeDescipt.includes( descriptText )) {
-    return resultList = bookThree;
-  } else {
-    return 'No se encontró ese libro';
-  }
-}
-
-const formSection = document.querySelector('.new-form-act');
-const addBook = document.querySelector('.item');
-addBook.addEventListener( 'click', () => {
-
-  if( formSection.classList.contains('collapsed') ) {
-    return formSection.classList.remove('collapsed');
-  } else {
-    return formSection.classList.add('collapsed');
-  }
-});
-
-const pictureInput = document.querySelector('.pictute-input').value;
+//elements
+//inputs and buttons of new-form 
 const titleInput = document.querySelector('.title-input').value;
+const autorInput = document.querySelector('.autor-input').value;
 const descriptInput = document.querySelector('.descript-input').value;
+const requireInputs = [titleInput, descriptInput];
+const addFormButton = document.querySelector('.add-button-act');
+const cancelButton = document.querySelector('.button-cancel');
 const labelError = document.querySelector('.error-label');
 
-const requireInputs = [pictureInput, titleInput, descriptInput];
+//root render books
+const itemCard = document.querySelector('.book-data');
+const resultList = document.querySelector('.list');
 
-const addBookButton = document.querySelector('.add-button-act');
-addBookButton.addEventListener( 'click', () => {
-    if( pictureInput === '' || titleInput === '' || descriptInput === '' ) {
-      return labelError.innerHTML = 'Debe rellenar los campos obligatorios';
-    }
-});
-
-const SearchDescript = document.querySelector('.search-description').value;
+//inputs and buttons of search
+const searchIcon = document.querySelector('.plusIcon');
+const searchTitle = document.querySelector('.search-title').value;
 const searchAutor = document.querySelector('.search-autor').value;
 const searchButton = document.querySelector('.search-button');
 const serchError = document.querySelector('.error-search');
 
-searchButton.addEventListener( 'click', () => {
-  if( SearchDescript === '' && searchAutor === '' ) {
-    alert('Debe rellenar los campos obligatorios');
-    SearchDescript = '';
-    searchAutor = '';
+// renderBook function
+const renderBook = ( title, autor, description ) => {
+  `<li class="card">
+      <article>
+        <h3 class="card-title"><strong>${ title }:</strong> Manual de estilo para el desarrollo ágil de software</h3>
+        <h4 class="card-autor">${ autor }</h4>
+        <p class="card-description">${ description }</p>
+      </article>
+    </li>`;
+}
+
+itemCard.innerHTML = renderBook( titleInput, autorInput, descriptInput );
+
+//Display form
+const formSection = document.querySelector('.new-form-act');
+const showForm = () => formSection.classList.remove('collapsed');
+const hideForm = () => formSection.classList.add('collapsed');
+const addButton = document.querySelector('.item');
+
+const handleForm = (event) => {
+  event.preventDefault();
+  if( formSection.classList.contains('collapsed') ) {
+    searchIcon.innerHTML = 'highlight_off';
+    return showForm();
+  } else {
+    searchIcon.innerHTML = 'add_circle';
+    return hideForm();
   }
-})
+}
+addButton.addEventListener( 'click', handleForm );
+
+//validate search / Require inputs 
+addFormButton.addEventListener( 'click', () => {
+  if( titleInput !== '' ) {
+    books.push(
+      {
+        title: `${titleInput}`,
+        autor: `${autorInput}`,
+        description: `${descriptInput}`,
+      },
+    );
+  } else {
+      return labelError.innerHTML = 'debe agregar el título';
+  };
+});
+
+// form cancel
+cancelButton.addEventListener( 'click', () => {
+  if( titleInput !== '' || autorInput !== '' || descriptInput !== '' ) {
+    titleInput = '';
+    autorInput = '';
+    descriptInput = '';
+    pictureInput = '';
+    hideForm();
+  };
+});
+
+// Autenticate bookSearch and throw error
+searchButton.addEventListener( 'click', () => {
+  if( books.title.includes( searchTitle) || books.autor.includes( searchAutor)) {
+    console.log(`si está ese libro`);
+  }
+  if( searchTitle === '' && searchAutor === '' ){
+    return serchError.innerHTML += 'Debe rellenar alguno de los campos';
+  }
+  else {
+    return 'No se encontró ese libro';
+  };
+  searchTitle = '';
+  searchAutor = '';
+});
